b4075dfcc37e33647d07e1f028073a717638035c
Deneth Weerasinghe <deneth@hotmail.co.uk>
2025-10-14 22:11:14 +0100

    Reworked hydration bar to use repeated sprites

--- src/main/java/com/algolrithm/toughsurvival2/client/HUDHandler.java
+++ src/main/java/com/algolrithm/toughsurvival2/client/HUDHandler.java
@@ -15,17 +15,16 @@ import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import net.neoforged.neoforge.common.NeoForge;
 
 public class HUDHandler {
-
-    static final ResourceLocation SPRITES = ResourceLocation.fromNamespaceAndPath(ToughSurvival2.MODID, "textures/icons/sprite_sheet.png");
-    static final int SPRITE_SHEET_HEIGHT = 81;
-    static final int SPRITE_SHEET_WIDTH = 80;
+    private static final ResourceLocation HYDRATION_EMPTY = ResourceLocation.fromNamespaceAndPath(ToughSurvival2.MODID, "hydration_empty");
+    private static final ResourceLocation HYDRATION_HALF = ResourceLocation.fromNamespaceAndPath(ToughSurvival2.MODID, "hydration_half");
+    private static final ResourceLocation HYDRATION_FULL = ResourceLocation.fromNamespaceAndPath(ToughSurvival2.MODID, "hydration_full");
 
     public static void register(RegisterGuiLayersEvent event) {
         event.registerAbove(VanillaGuiLayers.FOOD_LEVEL, HydrationOverlay.ID, new HydrationOverlay());
     }
-
     public static abstract class Overlay implements GuiLayer {
         public abstract void render(Minecraft mc, Player player, GuiGraphics guiGraphics, int top, int right, int left);
+
         @Override
         public void render(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
             Minecraft mc = Minecraft.getInstance();
@@ -37,20 +36,19 @@ public class HUDHandler {
 
             render(mc, mc.player, guiGraphics, top, right, left);
         }
-
         public boolean shouldRenderOverlay(Minecraft mc, Player player, GuiGraphics guiGraphics) {
             return !mc.options.hideGui && mc.gameMode != null && mc.gameMode.canHurtPlayer();
         }
-    }
 
+    }
     public static class HydrationOverlay extends Overlay {
         public static final ResourceLocation ID = ResourceLocation.fromNamespaceAndPath(ToughSurvival2.MODID, "hydration_overlay");
 
         @Override
         public void render(Minecraft mc, Player player, GuiGraphics guiGraphics, int top, int right, int left) {
-            float testValue = 5.0F;
-            int x = right / 2 + 10;
+            int x = right / 2 + 10 + 81;
             int y = top - 49;
+            float testValue = 5.0F;
 
             HUDRenderEvent.HydrationBar hydrationBarEvent = new HUDRenderEvent.HydrationBar(testValue, x, y, guiGraphics);
             if(player.getVehicle() instanceof LivingEntity) {
@@ -62,6 +60,8 @@ public class HUDHandler {
             // Moves the air bar up, so it won't be overdrawn by the hydration bar
             mc.gui.rightHeight += 10;
 
+
+
             drawHydrationBar(hydrationBarEvent, player);
         }
     }
@@ -69,7 +69,15 @@ public class HUDHandler {
     private static void drawHydrationBar(HUDRenderEvent.HydrationBar event, Player player) {
         drawHydrationBar(player, event.guiGraphics, event.x, event.y, event.hydration);
     }
+    // TODO see what happens if change access modifier to private
     public static void drawHydrationBar(Player player, GuiGraphics guiGraphics, int right, int top, float hydration) {
-        guiGraphics.blit(RenderPipelines.GUI_TEXTURED, SPRITES, right, top, 0, 0, 81, 9, SPRITE_SHEET_WIDTH, SPRITE_SHEET_HEIGHT);
+        right -= 9;
+        int x;
+        int y = top;
+
+        for (int i = 0; i < 10; i++) {
+            x = right - (i * 8);
+            guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED, HYDRATION_FULL, x, y ,9, 9);
+        }
     }
 }
