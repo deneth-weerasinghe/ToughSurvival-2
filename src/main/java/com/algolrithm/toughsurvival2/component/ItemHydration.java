825b1052a0a52811525787ee3c03b64c2e360dd2
Deneth Weerasinghe <deneth@hotmail.co.uk>
2025-11-05 21:12:12 +0000

    Switched to implementing ConsumableListener instead of overriding finishUsingItem

--- src/main/java/com/algolrithm/toughsurvival2/component/ItemHydration.java
+++ src/main/java/com/algolrithm/toughsurvival2/component/ItemHydration.java
@@ -1,49 +1,37 @@
 package com.algolrithm.toughsurvival2.component;
 
+import com.algolrithm.toughsurvival2.player.Hydration;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.component.ConsumableListener;
+import net.minecraft.world.level.Level;
 
 import javax.annotation.concurrent.Immutable;
-import java.util.Objects;
 
 @Immutable
-public class ItemHydration {
+public record ItemHydration(int hydration) implements ConsumableListener {
     public static final Codec<ItemHydration> CODEC = RecordCodecBuilder.create(
             instance -> instance
-                    .group(Codec.INT.optionalFieldOf("item_hydration", 0).forGetter(ItemHydration::getHydration))
+                    .group(Codec.INT.optionalFieldOf("item_hydration", 0).forGetter(ItemHydration::hydration))
                     .apply(instance, ItemHydration::new)
     );
     public static final StreamCodec<ByteBuf, ItemHydration> STREAM_CODEC = StreamCodec.composite(
             ByteBufCodecs.INT,
-            ItemHydration::getHydration,
+            ItemHydration::hydration,
             ItemHydration::new
     );
 
-    private int hydration;
-
-    public ItemHydration(int value) {
-        this.hydration = value;
-    }
-
-    public int getHydration() {
-        return hydration;
-    }
-
-    public void setHydration(int hydration) {
-        this.hydration = hydration;
-    }
-
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        return obj instanceof ItemHydration a && this.hydration == a.hydration;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.hydration);
+    public void onConsume(Level level, LivingEntity entity, ItemStack stack, Consumable consumable) {
+        if(entity instanceof Player player && !level.isClientSide()) {
+            Hydration.incrementHydration(player, stack.get(ModComponents.ITEM_HYDRATION).hydration());
+        }
     }
 }
--- src/main/java/com/algolrithm/toughsurvival2/item/ModDrinkableItem.java
+++ src/main/java/com/algolrithm/toughsurvival2/item/ModDrinkableItem.java
@@ -2,14 +2,9 @@ package com.algolrithm.toughsurvival2.item;
 
 import com.algolrithm.toughsurvival2.component.ItemHydration;
 import com.algolrithm.toughsurvival2.component.ModComponents;
-import com.algolrithm.toughsurvival2.player.Hydration;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.Consumables;
-import net.minecraft.world.level.Level;
 
 
 public class ModDrinkableItem extends Item {
@@ -18,15 +13,5 @@ public class ModDrinkableItem extends Item {
                 .component(ModComponents.ITEM_HYDRATION.get(), new ItemHydration(hydration))
                 .component(DataComponents.CONSUMABLE, Consumables.defaultDrink().build())
         );
-    }
-
-    @Override
-    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity livingEntity) {
-        ItemStack itemStack = super.finishUsingItem(stack, level, livingEntity);
-        if(!itemStack.has(ModComponents.ITEM_HYDRATION)) return itemStack;
-        if(!level.isClientSide() && livingEntity instanceof Player) {
-            Hydration.incrementHydration((Player) livingEntity, stack.get(ModComponents.ITEM_HYDRATION).getHydration());
-        }
-        return itemStack;
     }
 }
